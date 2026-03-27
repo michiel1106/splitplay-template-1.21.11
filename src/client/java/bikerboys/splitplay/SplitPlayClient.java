b212314298cafd28a8ac009c03f183797910e91f
michiel1106 <michielimmink@gmail.com>
2026-03-27 09:52:00 +0100

    Initial commit

---
+++ src/client/java/bikerboys/splitplay/SplitPlayClient.java
@@ -0,0 +1,10 @@
+package bikerboys.splitplay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SplitPlayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
